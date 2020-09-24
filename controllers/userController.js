b08ae7b8325136dbbe78d2f06b961a104f5e38b4
ddddam1 <50946249+ddddam1@users.noreply.github.com>
2020-09-25 00:50:11 +0900

    2.18

---
+++ controllers/userController.js
@@ -0,0 +1,3 @@
+export const join = (req, res) => res.render("Join", {pageTitle: "Join"});
+export const login = (req, res) => res.render("Login", {pageTitle: "Login"});
+export const logout = (req, res) => res.render("Logout", {pageTitle: "Logout"});
---
+++ controllers/videoController.js
@@ -0,0 +1,12 @@
+export const home = (req, res) => res.render("home", {pageTitle: "Home"});
+
+export const search = (req, res) => {
+    const {query: {term: searchingBy}} = req;
+    res.render("search", {pageTitle: "Search", searchingBy});
+};
+
+export const videos = (req, res) => res.render("videos", {pageTitle: "Videos"});
+export const upload = (req, res) => res.render("upload", {pageTitle: "Upload"});
+export const videoDetail = (req, res) => res.render("Video Detail", {pageTitle: "Video Detail"});
+export const editVideo = (req, res) => res.render("Edit Video", {pageTitle: "Edit Video"});
+export const deleteVideo = (req, res) => res.render("Delete Video", {pageTitle: "Delete Video"});
